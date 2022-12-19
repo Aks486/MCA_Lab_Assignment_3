e3a170e783028cc612682eca409ac2117a40f142
Akshay M Acharya <80585215+Aks486@users.noreply.github.com>
2022-12-19 20:14:38 +0530

    Add files via upload

---
+++ Q1/Code/Q1/Q1.ino
@@ -0,0 +1,52 @@
+//I2C library
+#include <Wire.h>
+#define i2c_addr 0x50
+#define max_addr 20
+
+void writeEeprom(int address, byte val)
+{ 
+  //Framing |start|i2c_addr|address to write(MSB)|address to write (LSB)|data (8-bits)|stop bit|
+  Wire.beginTransmission(i2c_addr);
+  Wire.write((int)(address >> 8));   
+  Wire.write((int)(address & 0xFF)); 
+  Wire.write(val);
+  Wire.endTransmission();
+  delay(5);
+}
+
+byte readEeprom(int address)
+{ 
+  byte rcvData = 0;
+  Wire.beginTransmission(i2c_addr);
+  Wire.write((int)(address >> 8));   
+  Wire.write((int)(address & 0xFF));
+  Wire.endTransmission();
+  Wire.requestFrom(i2c_addr, 1);
+  rcvData =  Wire.read();
+  return rcvData;
+}
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  unsigned int addr=0;
+  //Write to address
+  byte a[]={12,25,42,34,89,100,232,76,19,205,2,5,41,74,82,110,201,167,69,208};
+  //write the array to eeprom starting address 0
+  for(addr=0;addr<max_addr;addr++)
+    { 
+      writeEeprom(addr,a[addr]);
+    }
+   //read the array from the eeprom starting address 0
+  for(addr=0;addr<max_addr;addr++)
+    {
+      Serial.println("Value at Address "+(String)addr + ": " +readEeprom(addr));
+      delay(100);
+    } 
+    Serial.println();
+    Serial.println("----Read/Write Complete----");
+    while(1);
+  }
